54923066707548dc2049cec0d78e1ba5a6c43e76
Emily Irvine <emilymirvine@gmail.com>
2015-07-24 09:01:43 -0400

    Working assignment 2. Computes the inverse of a matrix if that inverse had not been previously computed. Uses caching to increase computational efficiency.

--- ProgrammingAssignment2/cachematrix.R
+++ ProgrammingAssignment2/cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Creating a matrix object that can cache its inverse without looping
+## to reduce computational costs.
+## This function sets the value for the matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	m <- NULL
+	set <- function(y) {
+	x <<- y
+	m <<- NULL
+	}
+	
+	get <- function() x
+	setmatrix <- function(inv) m <<- inv
+	getmatrix <- function() m
+	list(set = set, get = get,
+	setmatrix = setmatrix,
+	getmatrix = getmatrix)
 }
 
-
-## Write a short comment describing this function
+## Helper function to compute the inverse of the matrix created above.
+## It first checks if the inverse has already been computed, if so it 
+## returns that value, otherwise it computes the inverse.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	m <- x$getmatrix()
+	# checks if cacheSolve has already been executed
+	if(!is.null(m)) {
+		message("Getting cached data")
+		return(m)
+	}
+	matrix <- x$get()
+	m <- solve(matrix)
+	x$setmatrix(m)
+	m # returns the inverse
 }
